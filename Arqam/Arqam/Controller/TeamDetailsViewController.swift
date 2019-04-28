e42376e19ec2c9404f71b1a6ba6af3515f6bf002
Gamal Gamaleldin <gamal.m.esmail@gmail.com>
2019-04-28 13:45:29 +0200

    Show favourite team details.

--- Arqam/Arqam/Controller/TeamDetailsViewController.swift
+++ Arqam/Arqam/Controller/TeamDetailsViewController.swift
@@ -10,26 +10,42 @@ import Foundation
 import UIKit
 import CoreData
 
-class TeamDetailsViewController : UIViewController {
+class TeamDetailsViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     //MARK: Injections
     var dataController : DataController!
     var theFavouriteTeam : FavTeam!
     var isFavouriteTeam : Bool!
     
+    //MARK: Outlets
+    @IBOutlet weak var teamName: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     //MARK: Instance Variables
     var favTeamMembers: [FavTeamMember] = []
+    @IBOutlet weak var activity: UIActivityIndicatorView!
     
     //MARK: View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Setting TableView
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.allowsSelection = false
+        
         //Fetch CoreData
         if(isFavouriteTeam){
-          fetchFavourite()
-            for favTeam in favTeamMembers {
-                print(favTeam.name)
-            }
+            teamName.text = theFavouriteTeam.name
+            activity.isHidden = false
+            activity.startAnimating()
+            fetchFavourite()
         }
+        sortTeamMembers() //manager - goalkeepers - defenders - midfielders - attackers
+        tableView.reloadData()
+        activity.isHidden = true
+        activity.stopAnimating()
     }//closing of view did load
     
     //MARK: Fetch CoreData
@@ -70,6 +86,54 @@ class TeamDetailsViewController : UIViewController {
             
         }//end of if let
     }//end of fetch favourite func
+    
+    //MARK: SortTeam
+    func sortTeamMembers(){
+        if isFavouriteTeam{
+            var favManager = FavTeamMember()
+            var favGoalies: [FavTeamMember] = []
+            var favDefenders: [FavTeamMember] = []
+            var favMids: [FavTeamMember] = []
+            var favAttackers: [FavTeamMember] = []
+            
+            for teamMember in favTeamMembers {
+                if teamMember.role == "COACH" {favManager = teamMember}
+                else if teamMember.position == "Goalkeeper" {favGoalies.append(teamMember)}
+                else if teamMember.position == "Defender" {favDefenders.append(teamMember)}
+                else if teamMember.position == "Midfielder" {favMids.append(teamMember)}
+                else {favAttackers.append(teamMember)}
+            }//closing of for loop
+            favTeamMembers = []
+            favTeamMembers.append(favManager)
+            favTeamMembers.append(contentsOf: favGoalies)
+            favTeamMembers.append(contentsOf: favDefenders)
+            favTeamMembers.append(contentsOf: favMids)
+            favTeamMembers.append(contentsOf: favAttackers)
+            
+        }//closing of favourite team
+    }
+    
+    //MARK: Table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.favTeamMembers.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let teamMemberCell = tableView.dequeueReusableCell(withIdentifier: "playerCell") as! PlayerCell
+        teamMemberCell.backgroundColor = UIColor.white
+        teamMemberCell.textLabel?.textColor = UIColor(red: 0.0, green: 0.715, blue: 0.226, alpha: 1)
+        teamMemberCell.layer.borderWidth = 5
+        teamMemberCell.layer.borderColor = UIColor(red: 0.0, green: 0.715, blue: 0.226, alpha: 0.6).cgColor
+        teamMemberCell.clipsToBounds = true
+        if(isFavouriteTeam){
+            let teamMember = favTeamMembers[(indexPath as NSIndexPath).row]
+            teamMemberCell.name.text = teamMember.name
+            teamMemberCell.nationality.text = teamMember.nationality
+            teamMemberCell.position.text = (teamMember.role == "COACH") ? teamMember.role : teamMember.position
+            
+        }
+        
+        return teamMemberCell
+    }
 
-}//end
+}//closing of class.
 
---
+++ Arqam/Arqam/View/Customized Cells/PlayerCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerCell.swift
+//  Arqam
+//
+//  Created by Gamal Gamaleldin on 4/28/19.
+//  Copyright © 2019 el3ankaboot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlayerCell : UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var position: UILabel!
+    @IBOutlet weak var nationality: UILabel!
+    
+}
